723005972deb579dfec3d7d68992e8506be4f7ae
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2025-03-23 00:44:34 -0400

    First version of bubble sort. Needs user input
    Bubble sort seems to work. Uses unoptimized algorithm, may change later.
    Needs better named variables and user input.

--- 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
+++ 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
@@ -1,1 +1,73 @@
-int main(){}
+#include <stdio.h>
+
+#define SIZE 10
+
+int a[SIZE] = {2, 6, 4, 8, 10, 12, 89, 68, 45, 37};
+
+void bubble_sort(int (*comparison_function) (int, int), int numbers[], int size);
+int ascending(int first, int second);
+int descending(int first, int second);
+
+int main() {
+
+  printf("Data items in original order\n");
+
+  for (int i = 0; i < SIZE; i++)
+    printf("%5d", a[i]);
+  printf("\n");
+
+  bubble_sort(descending, a, SIZE);
+
+  printf("Data items in sorted order\n");
+
+  for (int i = 0; i < SIZE; i++)
+    printf("%5d", a[i]);
+  printf("\n");
+
+}
+
+/** @brief Bubble sort algorithm sorts the array numbers either ascending
+ *         or descending depending on what function is passed for comparison_function.
+ *
+ *         Implemented based on the unoptimized pseudocode implementation in wikipedia's bubble sort article.
+ *
+ *  @param comparison_function  The function that compares to determine if a swap must be done.
+ *  @param numbers  The array to sort.
+ *  @param size The size of the array numbers.
+ */
+void bubble_sort(int (*comparison_function) (int, int), int numbers[], int size) {
+  int swapped = 0;
+
+  do {
+    swapped = 0;
+
+    //Loop through all elements, comparing and swapping if necessary
+    for (int i = 0; i < size - 1; i++) {
+      if ((*comparison_function) (numbers[i], numbers[i + 1])) {
+        int temp = numbers[i];
+        numbers[i] = numbers[i + 1];
+        numbers[i + 1] = temp;
+        swapped = 1;
+      }
+    }
+  } while (swapped); //Takes one extra pass to know the array is sorted
+
+  return;
+}
+
+/** @brief ascending returns 1 if a swap must be done to sort in ascending order.
+ *  @param first  The element that comes first in the array.
+ *  @param second The element that comes second in the array.
+ *  @return 0 if second is larger than first, that is, it is ascending already, and 1 if it is not ascending and a swap must be done.
+ */
+int ascending(int first, int second) {
+  return first > second;
+}
+/** @brief descending returns 1 if a swap must be done to sort in descending order.
+ *  @param first  The element that comes first in the array.
+ *  @param second The element that comes second in the array.
+ *  @return 0 if second is smaller than first, that is, it is descending already, and 1 if it is not descending and a swap must be done.
+ */
+ int descending(int first, int second) {
+  return second > first;
+}
